3cdb223830975a3cd158880e556dd5cadc05c191
Deng <dpan968@gmail.com>
2020-07-09 15:26:47 +1000

    Part3 - Templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
+posts = [
+    {
+        'author': 'Deng Pan',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '7th July 2020'
+    },
+
+    {
+        'author': 'Deng Pan',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '8th July 2020'
+    }
+
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
