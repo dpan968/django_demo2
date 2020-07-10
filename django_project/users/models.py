6488018709fd1ddc8fdfe8ac2d335cde46492739
Deng <dpan968@gmail.com>
2020-07-10 13:54:47 +1000

    Part 9 Update User Profile

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # models.CASCADE is a one way thing, i.e. when an user is deleted, the profile is also deleted, but not the other way around.
 
@@ -10,3 +11,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
